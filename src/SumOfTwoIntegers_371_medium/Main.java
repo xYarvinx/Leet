87ddba6ffcd78dc90c8934449ed0ef078f632d48
xYarvinx <otebator@gmail.com>
2024-04-14 23:48:14 +1000

    refactor package

--- src/SumOfTwoIntegers_371_medium/Main.java
+++ src/SumOfTwoIntegers_371_medium/Main.java
@@ -3,6 +3,7 @@ package SumOfTwoIntegers_371_medium;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution c = new Solution();
+        System.out.print(c.getSum(1,2));
     }
 }
